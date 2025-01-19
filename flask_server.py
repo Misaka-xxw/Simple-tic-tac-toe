ab4be83637a5d7daaf6e6a5d5235a5fc146e292b
Misaka-xxw <110085579+Misaka-xxw@users.noreply.github.com>
2025-01-19 22:27:37 +0800

    Update html <-link-> flask (not completed yet)

--- flask_server.py
+++ flask_server.py
@@ -3,11 +3,14 @@ import json
 from flask import Flask, request, jsonify, send_file, redirect, send_from_directory, session
 from flask_cors import CORS
 
+from Algorithm import Algorithm
+
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
 app.config['SESSION_TYPE'] = 'filesystem'
 CORS(app)
 client_ip=None
+board=Algorithm()
 
 # static resources
 @app.route('/static/<filename>', methods=['GET'])
@@ -45,6 +48,24 @@ def get_pages(filename):
     global client_ip
     print(filename)
     return send_from_directory('./view', filename)
+
+@app.route('/api/drop', methods=['POST'])
+async def drop():
+    if request.content_type != 'application/json':
+        return jsonify({"error": "Content type must be application/json",
+                        "status": "error"}), 400
+    data = request.get_json()
+    print(data)
+    if not data:
+        return jsonify({"message": "Invalid JSON", "status": "error"}), 400
+    player_id=data.get("player_id")
+    auto=data.get("auto")
+    mode=data.get("mode")
+    if auto==0: # 不是机器第一步
+        chess_id = data.get("chess_id")
+        board.play_step(player_id,auto=False,x=chess_id/3,y=chess_id%3)
+        if mode==1:
+            board.play_step(3-player_id)
 
 
 if __name__ == '__main__':
